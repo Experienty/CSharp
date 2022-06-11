c6e67366d082eb91ea45e9853790840fd1b430bd
Experienty/CSharp <extremoexperienty@gmail.com>
2022-06-11 16:30:53 -0300

    My tweleven Commit

---
+++ Father.cs
@@ -0,0 +1,37 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+//Exemplo do metodo :base() abaixo
+namespace Fat
+{
+    public class Exemplo1
+    {         
+        public Exemplo1()
+        {
+            Console.WriteLine("A\n");
+        }
+        public Exemplo1(string firts)
+        {
+        	Console.WriteLine($"Primeiro e unico {firts}\n");
+        }
+    }
+    class Exemplo:Exemplo1
+    {
+    	public Exemplo()
+    	{
+    		Console.WriteLine("B\n");
+    	}
+    	public Exemplo(string firts):base(firts)
+    	{
+    		Console.WriteLine($"Segundo e unico {firts}\n");
+    	}
+    }
+    class Program
+    {
+    	static void Main()
+        {
+        	Exemplo1 e = new Exemplo1("God");
+        	Exemplo a = new Exemplo("Jesus");
+        }
+    }
+}*/
