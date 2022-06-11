aa2c29075ea923546eb42c9c793aa9c7d19296eb
Experienty/CSharp <extremoexperienty@gmail.com>
2022-06-11 14:14:46 -0300

    My Tem Commit

---
+++ Pares.cs
@@ -0,0 +1,23 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Number
+{
+    public class Pares 
+    {         
+         static void Main()
+         {
+         	int p = 0;
+         	for(int i = 0; i < 5; i++){
+         		Console.Write("digite os valores: ");
+         		int.TryParse(Console.ReadLine(), out int X);
+         		Console.Clear();
+         		if(X%2 == 0){
+         			p++;
+         		}
+         	}
+         	Console.WriteLine($"{p} Valores Pares\n");
+         }
+    }
+}*/
