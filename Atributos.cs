2b0cc9e1f519588864e3d6a54b190e2c2028bf52
Experienty/CSharp <extremoexperienty@gmail.com>
2022-06-11 12:22:33 -0300

    Meu Sexto Commit

---
+++ Atributos.cs
@@ -0,0 +1,39 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Atributo
+{
+	public class Atributos
+	{
+		public string Name;
+		public string SubName;
+		public int Idade;
+
+		public Atributos(string name, string subName, int idade)
+		{
+			Name = name;
+			SubName = subName;
+			Idade = idade;
+		}
+		public Atributos()
+		{
+			Console.WriteLine("Sem Atribuições");
+		}
+	}
+	public class Program
+	{
+		static void Main()
+		{
+			//Exemplo da classe "Atributos" sem Atribuições nos Parametros
+			//Atributos a = new Atributos();
+			//Exemplo com Atribuição nos Parametros
+			//Atributos a = new Atributos("Hipnus", "Lord", 243);
+			//Exemplo dando valores aos Atributos sem colocar Parametros
+			/*a.Name = "Thanatus";
+			a.SubName = "Lord";
+			a.Idade = 243;
+			Console.WriteLine($"Nome: {a.Name} Sobre Nome: {a.SubName} & Idade: {a.Idade}");*
+		}
+	}
+}*/
