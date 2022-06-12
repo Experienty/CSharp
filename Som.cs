1c8ba7721c09ab70742a17b5595dafba3e564a9c
Experienty/CSharp <extremoexperienty@gmail.com>
2022-06-12 10:30:10 -0300

    add Som.cs

---
+++ Som.cs
@@ -0,0 +1,28 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Somar
+{
+    public class Som 
+    {         
+        public double calculo(double x1, double x2)
+        {
+        	return (x1%x2);
+        }
+        public double cal(double x1, double x2)
+        {
+        	return (x1/x2);
+        }
+    }
+    
+    public class Program
+    {
+    	static void Main()
+        {
+        	Som s = new Som();
+        	Console.WriteLine(s.calculo(3,2));
+        	Console.WriteLine($"{s.cal(4,5)}");
+        }
+    }
+}*/
