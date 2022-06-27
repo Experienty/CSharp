62b425d34d40625085a87c357c73fc605b8c48e7
Experienty/CSharp <extremoexperienty@gmail.com>
2022-06-27 13:00:14 -0300

    My Firfteen Commit

---
+++ Cartey.cs
@@ -0,0 +1,38 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Cart
+{
+    public class Cartey 
+    { 
+    	public int dinheiro = 50;
+        public void Acresentar(int valor)
+        {
+        	dinheiro += valor;
+        }
+        public void Retirar(int valor)
+        {
+        	dinheiro -= valor;
+        }
+        public int Mostrar()
+        {
+            return dinheiro;
+        }
+        public void AlterarDinheiro(int val)
+        {
+        	dinheiro += val;
+        }
+    }
+    class Pro
+    {
+    	static void Main()
+    	{
+    		Cartey c = new Cartey();
+    		/*c.Acresentar(50);
+    		c.Retirar(50);*
+    		c.AlterarDinheiro(100);
+    		Console.WriteLine(c.Mostrar());
+    	}
+    }
+}*/
