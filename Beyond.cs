55603ed21c5371261cc60a70d09838b90be26ce0
Experienty/CSharp <extremoexperienty@gmail.com>
2022-06-28 13:09:34 -0300

    Adicionando o Beyond.cs novamente

---
+++ Beyond.cs
@@ -0,0 +1,34 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Beyond
+{
+    class Village
+    {         
+        protected readonly string name = "Saitama";
+        protected readonly int id = 243;
+        protected readonly string pais = "Japão";
+        public virtual void Person()
+        {
+            Console.WriteLine($"Seu Nome é: {name} Sua idade {id} Seu Pais {pais}\n");
+        }
+    }
+    class City:Village
+    {
+    	public override void Person()
+    	{
+    		string mac = "fc:27:dt:h7:8j9";
+    		base.Person();
+    		Console.WriteLine($"Seu Mac é: {mac}");
+    	}
+    }
+    class Program
+    {
+    	static void Main()
+    	{
+    		City c = new City();
+    		c.Person();
+    	}
+    }
+}*/
