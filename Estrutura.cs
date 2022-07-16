60532c9f9b27ce5b17a73424d9db4dbc08dc614f
ex <extremoexperienty@gmail.com>
2022-07-16 12:42:08 -0300

    My Twity One Commit

---
+++ Estrutura.cs
@@ -0,0 +1,30 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Whily
+{
+    public class Estrutura 
+    {         
+        static void Main()
+        {
+        	double idade, soma = 0, media;
+        	int cont = 0;
+        	idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        	
+        	while(idade >= 0){
+        		soma += idade;
+        		cont += 1;
+            idade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        	}
+        	if(cont == 0){
+        		Console.Write(" Impossivel de Calcular");
+        	}else{
+        		media = soma/ cont;
+        		Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        		//Console.ReadLine();
+        	}
+        }
+    }
+}*/
