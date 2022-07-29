314dbcfb9fc1056735c2a15634f958790714b98b
Experienty/CSharp <extremoexperienty@gmail.com>
2022-07-29 11:03:26 -0300

    My twity three commit

---
+++ Calcutale.cs
@@ -0,0 +1,28 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Simple
+{
+    class Calcutale 
+    {         
+        static void Main()
+        {
+        	Console.Write("digite o numero: ");
+        	int.TryParse(Console.ReadLine(), out int x);
+        	Console.Clear();Console.Write("digite a quantidade: ");
+        	int.TryParse(Console.ReadLine(), out int y);
+        	Console.Clear();Console.Write("digige o valor: ");
+        	double.TryParse(Console.ReadLine(), out double z);
+        	Console.Clear();Console.Write("digite o segundo numero: ");
+        	int.TryParse(Console.ReadLine(), out int a);
+        	Console.Clear();Console.Write("digite a quantidade: ");
+        	int.TryParse(Console.ReadLine(), out int b);
+        	Console.Clear();Console.Write("digite o valor: ");
+        	double.TryParse(Console.ReadLine(), out double c);
+        	Console.Clear();
+        	double total = (y * z)+(b*c);
+        	Console.WriteLine($"VALOR A PAGAR {total:c}");
+        }
+    }
+}*/
