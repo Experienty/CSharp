bcdc61e745f21d153bec5247e0b9c75da421cfb4
Experienty/CSharp <extremoexperienty@gmail.com>
2022-07-29 12:31:08 -0300

    My twity five commit

---
+++ Triangulo.cs
@@ -0,0 +1,16 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Area
+{
+    public class Triangulo 
+    { 
+        Console.WriteLine("Calcular a area do Triangulo");
+        Console.Write("Informe a base do triangulo: ");
+        double.TryParse(Console.ReadLine(), out double b);
+        Console.Write("Informe a altura do triangulo: ");
+        double.TryParse(Console.ReadLine(), out double a);
+        Console.WriteLine($"Area = {(b*a)/2}");
+    }
+}*/
---
+++ Yield.cs
@@ -0,0 +1,33 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Retorno
+{
+    class Yield 
+    {         
+        static void Main()
+        {
+        	WriteLine("Antes de chamar o Foo...");
+        	var foo = Foo();
+        	WriteLine("Depois de chamar o Foo...");
+        	
+        	foreach(var elem in foo){
+        		WriteLine($"Antes de Imprimir o elem {elem}...");
+        		WriteLine(elem);
+        		WriteLine($"Depois de Imprimir o elem {elem}...");
+        	}
+        }
+        static IEnumerable<int> Foo()
+        {
+        	WriteLine("Antes de iniciar o loop for...");
+        	for(int i = 0; i < 4; i++){
+        		WriteLine($"Antes do yield return {i}...");
+        		yield return i;
+        		WriteLine($"Depois do yield return {i}...");
+        	}
+        	WriteLine("Depois de Encerrar o loop for...");
+        }
+    }
+}*/
