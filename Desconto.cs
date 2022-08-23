28d78890cfebd54f9f2ec55b855b35de6d4adb40
Experienty/CSharp <extremoexperienty@gmail.com>
2022-08-22 21:54:30 -0300

    My add Commit

---
+++ Desconto.cs
@@ -0,0 +1,18 @@
+﻿/*using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Sm
+{
+    public class Somar 
+    {         
+        static void Main()
+        {
+        	int res=0,x=0,i;
+        	for(i = 1; x < 100; i++){
+        		res = i * (i + 1)/2;
+        	}
+        	Console.WriteLine($"A soma de numeros de 1 ate 100 é {res}");
+        }
+    }
+}*/
